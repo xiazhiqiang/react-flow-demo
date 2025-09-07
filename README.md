ec521ddf09d052b25d71023e43e426c45583cc8b
Miles <miles@MilesdeMacBook-Pro.local>
2025-09-07 23:05:21 +0800

    feat: init flow

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # react-flow-demo
+
 react-flow Demo
+
+## 本地开发
+
+- 基于 vite 初始化
+- 安装 reactflow@11
+- 支持 sass，npm i sass -D
+
+## 参考
+
+- [vite](https://cn.vite.dev/guide/)
+- [reactflow@11](https://v11.reactflow.dev/learn)
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import Home from "./components/Home";
+import Flow1 from "./components/Flow1";
+
+function App() {
+  return (
+    <>
+      {/* <Home /> */}
+      <Flow1 />
+    </>
+  );
+}
+
+export default App;
