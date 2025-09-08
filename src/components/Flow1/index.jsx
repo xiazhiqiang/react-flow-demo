ec67c1abc02c05832056929839470a09ced5c5e2
Miles <miles@MilesdeMacBook-Pro.local>
2025-09-09 01:09:20 +0800

    feat: 更新

--- src/components/Flow1/index.jsx
+++ src/components/Flow1/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import ReactFlow, {
   useNodesState,
   useEdgesState,
@@ -6,18 +6,28 @@ import ReactFlow, {
   MiniMap,
   Controls,
   Background,
+  SelectionMode,
   Panel,
   NodeToolbar,
   NodeResizer,
 } from "reactflow";
 
 import "reactflow/dist/style.css";
+import "./index.scss";
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "1" },
+    type: "input",
+    style: { backgroundColor: "#ff0072", color: "white" },
+  },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" }, type: "output" },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges = [
+  { id: "e1-2", source: "1", target: "2", label: "to target" },
+];
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
@@ -28,21 +38,48 @@ export default function App() {
     [setEdges]
   );
 
+  const [variant, setVariant] = useState("dots");
+
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div className="flow-wrapper">
       <ReactFlow
+        fitView
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        defaultEdgeOptions={{ animated: true }}
+        panOnScroll // 空格 + 拖动鼠标、滚动、鼠标中键或鼠标右键
+        panOnDrag={[1, 2]} // 拖动鼠标创建
+        selectionOnDrag
+        selectionMode={SelectionMode.Partial} // 将仅部分选择的节点添加到选择中
       >
         <Controls />
-        <MiniMap />
-        {/* <Panel />
+        <MiniMap
+          zoomable
+          pannable
+          nodeStrokeWidth={10}
+          nodeColor={(node) => {
+            switch (node.type) {
+              case "input":
+                return "#6ede87";
+              case "output":
+                return "#6865A5";
+              default:
+                return "#ff0072";
+            }
+          }}
+        />
+        <Panel>
+          <div>variant:</div>
+          <button onClick={() => setVariant("dots")}>dots</button>
+          <button onClick={() => setVariant("lines")}>lines</button>
+          <button onClick={() => setVariant("cross")}>cross</button>
+        </Panel>
         <NodeToolbar />
-        <NodeResizer /> */}
-        <Background variant="dots" gap={12} size={1} />
+        <NodeResizer />
+        <Background variant={variant} gap={12} size={1} />
       </ReactFlow>
     </div>
   );
